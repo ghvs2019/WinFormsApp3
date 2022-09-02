e14c927180abe5cbcd0a2ec4a54a257236f0f8b0
ghvs2019 <77206458+ghvs2019@users.noreply.github.com>
2022-09-02 15:53:42 +0300

    v11

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Version 3";
+            this.label1.Text = "Version 4";
             // 
             // Form1
             // 
